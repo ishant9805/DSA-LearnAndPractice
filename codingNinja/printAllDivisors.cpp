eb78d3cda995e26f735e6f1761b91dc8fb0698f4
ishant9805 <ishant9805@gmail.com>
2023-09-07 16:35:39 +0530

    used a different approach

--- codingNinja/printAllDivisors.cpp
+++ codingNinja/printAllDivisors.cpp
@@ -1,20 +1,25 @@
 #include<iostream>
 #include <vector>
+#include <cmath>
 using namespace std;
 
-int sumOfAllDivisors(int n){
-	int k = 0;
-	while(n > 0)
-	{
-		for (int i = 1; i <= n; i++)
+int sumOfAllDivisors(int n)
+{
+    int k = 0;
+    while (n > 0)
+    {
+        for(int i = 1; i <= sqrt(n); i++)
         {
             if(n % i == 0)
             {
+                // cout << i << " ";
                 k += i;
+                if(i != n/i) k += n/i;
             }
         }
-        n--;        
-	}
+        n--;
+    }
+    
     return k;
 }
 
