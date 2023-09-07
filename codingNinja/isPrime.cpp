c68f6a735ffe62e3f9bb6e3addddc9020345a5a5
ishant9805 <ishant9805@gmail.com>
2023-09-07 16:42:19 +0530

    Solved isPrime 🎉🥳

---
+++ codingNinja/isPrime.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+bool isPrime(int n)
+{
+    for (int i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    if (isPrime(n)) cout << "Yes";
+    else cout << "No";
+    
+    return 0;
+}
