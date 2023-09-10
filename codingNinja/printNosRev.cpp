b5cdbedf61b2ad17c64310c53d2ee8157dfe2c5c
ishant9805 <ishant9805@gmail.com>
2023-09-10 10:17:44 +0530

    tried another solution by making custom function

--- codingNinja/printNosRev.cpp
+++ codingNinja/printNosRev.cpp
@@ -2,23 +2,22 @@
 #include<vector>
 using namespace std;
 
+void custom(int i, vector<int> &vec)
+{
+    if (i == 0)
+    {
+        return;
+    }
+    vec.push_back(i);
+    custom(i - 1, vec);
+    return;    
+}
 
 vector<int> printNos(int x) 
 {
-    int n = x;
-    vector<int> res;
-    vector<int> tmp;
-    if (n < 1)
-    {
-        return res;
-    }
-    res.push_back(n);
-    tmp = printNos(n-1);
-    for (int i = 0; i < tmp.size(); i++)
-    {
-        res.push_back(tmp[i]);
-    }
-    return res;
+    vector<int> v;
+    custom(x, v);
+    return v;
 }
 
 int main()
