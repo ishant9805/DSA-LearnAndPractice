69e5e62579e3c44241673a3050c03aa0fb8f60cc
ishant9805 <ishant9805@gmail.com>
2023-09-12 11:17:51 +0530

    Solved using recursion

--- codingNinja/reverseArray.cpp
+++ codingNinja/reverseArray.cpp
@@ -2,16 +2,28 @@
 #include<vector>
 using namespace std;
 
-vector<int> reverseArray(int n, vector<int> &nums)
+vector<int> reverseArrayLoop(int n, vector<int> &nums)
 {
     int tmp = 0;
     // [0, 1, 2, 3, 4, 5] n = 6
     for (int i = 0; i < n / 2; i++)
     {
-        tmp = nums[i];
-        nums[i] = nums[n - i - 1];
-        nums[n - i - 1] = tmp;
+        swap(nums[i], nums[n - i - 1]);
     }
+    return nums;
+}
+
+void reverseArrRec(int start, int end, vector<int> &nums)
+{
+    if (start > end)
+        return;
+    swap(nums[start], nums[end]);
+    return reverseArrRec(start + 1, end - 1, nums);
+}
+
+vector<int> reverseArray(int n, vector<int> &nums)
+{
+    reverseArrRec(0, n - 1, nums);
     return nums;
 }
 
