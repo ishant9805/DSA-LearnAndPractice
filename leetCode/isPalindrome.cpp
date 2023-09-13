c9026b0ce7da296cef46c1f0e0b3b02ab3acfdc7
ishant9805 <ishant9805@gmail.com>
2023-09-13 12:44:29 +0530

    Solved using Loops 🥳🎉

--- leetCode/isPalindrome.cpp
+++ leetCode/isPalindrome.cpp
@@ -2,39 +2,68 @@
 #include<string>
 using namespace std;
 
-bool isPal(int start, int end, string& str)
-{   
-    if (str[start] == ' ')
-    {
-        return isPal(start + 1, end, str);
-    }
-    if (str[end] == ' ')
-    {
-        return isPal(start, end - 1, str);
-    }
+// bool isPal(int start, int end, string& str)
+// {   
+//     if (str[start] == ' ')
+//     {
+//         return isPal(start + 1, end, str);
+//     }
+//     if (str[end] == ' ')
+//     {
+//         return isPal(start, end - 1, str);
+//     }
     
-    if (start < end)
+//     if (start < end)
+//     {
+//         if (str[start] != str[end])
+//         {
+//             return false;
+//         }
+//         return isPal(start + 1, end - 1, str);
+//     }
+//     return true;
+// }
+
+// bool isPalindrome(string& str) {
+//     string p = str;
+//     for (int i = 0; i < p.size(); i++)
+//     {
+//         p[i] = tolower(p[i]);    
+//         if (p[i] < 97 || p[i] > 122)
+//         {
+//             p.erase(p.begin() + i);
+//         }
+//     }
+//     return isPal(0, p.size() - 1, p);
+// }
+
+bool isPalindrome(string& str)
+{
+    int start = 0;
+    int end = str.size() - 1;
+    while (start <= end)
     {
-        if (str[start] != str[end])
+        if (!isalnum(str[start]))
+        {
+            start++;
+            continue;
+        }
+        if (!isalnum(str[end]))
+        {
+            end--;
+            continue;
+        }
+        if (tolower(str[start]) != tolower(str[end]))
         {
             return false;
         }
-        return isPal(start + 1, end - 1, str);
+        else
+        {
+            start++;
+            end--;
+        }        
     }
     return true;
-}
-
-bool isPalindrome(string& str) {
-    string p = str;
-    for (int i = 0; i < p.size(); i++)
-    {
-        p[i] = tolower(p[i]);    
-        if (p[i] < 97 || p[i] > 122)
-        {
-            p.erase(p.begin() + i);
-        }
-    }
-    return isPal(0, p.size() - 1, p);
 }
 
 int main()
