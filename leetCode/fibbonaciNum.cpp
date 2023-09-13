620ef286b72bcbb0807181b2d5bd2c54fd0b7480
ishant9805 <ishant9805@gmail.com>
2023-09-13 13:47:20 +0530

    Solved using Loop for reducing time complexity.

--- leetCode/fibbonaciNum.cpp
+++ leetCode/fibbonaciNum.cpp
@@ -2,42 +2,63 @@
 #include<vector>
 using namespace std;
 
-vector<int> gFN(int i, int j, vector<int> vec, int n)
-{
-    if (vec.size() == n)
-    {
-        return vec;
-    }
+// vector<int> gFN(int i, int j, vector<int> vec, int n)
+// {
+//     if (vec.size() == n)
+//     {
+//         return vec;
+//     }
     
-    vec.push_back(i + j);
-    i = j;
-    j = vec[vec.size() - 1];
-    vec = gFN(i, j, vec, n);
-    return vec;
-}
+//     vec.push_back(i + j);
+//     i = j;
+//     j = vec[vec.size() - 1];
+//     vec = gFN(i, j, vec, n);
+//     return vec;
+// }
 
-int generateFibonacciNumbers(int n) 
+// int generateFibonacciNumbers(int n) 
+// {
+//     vector<int> vec;
+//     vec.push_back(0);
+//     vec.push_back(1);
+//     int sum0 = 0;
+//     int i = 0;
+//     int j = 1;
+//     if (n == 1)
+//     {
+//         return 1;
+//     }
+//     else if (n == 0)
+//     {
+//         return 0;
+//     }
+//     else
+//     {
+//         vec = gFN(i, j, vec, n);
+//         sum0 = vec[n - 2] + vec[n - 1];
+//         return sum0;
+//     }
+// }
+
+int generateFibonacciNumbers(int n)
 {
-    vector<int> vec;
-    vec.push_back(0);
-    vec.push_back(1);
-    int sum0 = 0;
     int i = 0;
     int j = 1;
+    int a = 1;
+    int res = 0;
     if (n == 1)
     {
         return 1;
     }
-    else if (n == 0)
+    while (a < n)
     {
-        return 0;
+        res = i + j;
+        i = j;
+        j = res;
+        a++;
     }
-    else
-    {
-        vec = gFN(i, j, vec, n);
-        sum0 = vec[n - 2] + vec[n - 1];
-        return sum0;
-    }
+    return res;
+    
 }
 
 int main()
