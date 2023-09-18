352a62eb3d12e9cf88c63beace99e7edfc1dbfab
ishant9805 <ishant9805@gmail.com>
2023-09-18 16:03:36 +0530

    A bit larger code, but Solved 😎🎉

--- codingNinja/lowestHighestFreq.cpp
+++ codingNinja/lowestHighestFreq.cpp
@@ -4,10 +4,46 @@
 using namespace std;
 
 vector<int> getFrequencies(vector<int>& v) {
-    vector<int> res;
-    cout << "[";
-    
-    return res;
+    pair<int, int> max = {0, 0};
+    pair<int, int> min = {0, 999};
+    unordered_map<int, int> umpp;
+    for (int i = 0; i < v.size(); i++)
+    {
+        umpp[v[i]]++;
+    }
+    for(auto it : umpp)
+    {
+        if (max.second <= it.second)
+        {
+            if (max.second == it.second)
+            {
+                if (max.first > it.first)
+                {
+                    max.first = it.first;
+                }
+                
+            } else {            
+                max.first = it.first;
+                max.second = it.second;
+            }
+        }
+        if (min.second >= it.second)
+        {
+            if (min.second == it.second)
+            {
+                if (min.first > it.first)
+                {
+                    min.first = it.first;
+                }
+
+            } else { 
+                min.first = it.first;
+                min.second = it.second;
+            }
+        }
+        
+    }
+    return {max.first, min.first};
 }
 
 int main()
