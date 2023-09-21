1c7cc18cb2209e0dfd2a0338cc046ab9afdc06c4
ishant9805 <ishant9805@gmail.com>
2023-09-21 19:52:00 +0530

    works on coding ninja

--- codingNinja/findDuplicates.cpp
+++ codingNinja/findDuplicates.cpp
@@ -1,21 +1,41 @@
 #include<iostream>
 #include<vector>
+#include<array>
 using namespace std;
 
-int findDuplicate(vector<int>& arr, int n){
-	for (int i = 0; i < n; i++)
-	{
-        int j = 0;
-		while (j < i)
+// int findDuplicate(vector<int>& arr, int n){
+// 	for (int i = 0; i < n; i++)
+// 	{
+//         int j = 0;
+// 		while (j < i)
+//         {
+//             if (arr[i] == arr[j])
+//             {
+//                 return arr[i];
+//             }
+//             j++;
+//         }
+// 	}
+//     return -1;
+// }
+
+int findDuplicate(vector<int>& arr, int n)
+{
+    int hashArray[n] = 0;
+    for (int i = 0; i < n; i++)
+    {
+        hashArray[arr[i]]++;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (hashArray[i] > 1)
         {
-            if (arr[i] == arr[j])
-            {
-                return arr[i];
-            }
-            j++;
+            return i;
         }
-	}
-    return -1;
+        
+    }
+    
 }
 
 int main()
