f5c2786fc14eaf46c0a8606ac9568cef4c18a9b6
ishant9805 <ishant9805@gmail.com>
2023-09-27 21:10:53 +0530

    tried again but faied miserably

--- leetCode/isSorted.cpp
+++ leetCode/isSorted.cpp
@@ -33,24 +33,59 @@ bool isSorted(vector<int> a) {
     return true;
 }
 
-bool check(vector<int>& nums) {
-    for (int i = 0; i < nums.size(); i++)
+// bool check(vector<int>& nums) {
+//     for (int i = 0; i < nums.size(); i++)
+//     {
+//         vector<int> temp;
+//         temp = rotate(nums, i);
+//         if (isSorted(temp))
+//         {
+//             return true;
+//         }
+        
+//     }
+//     return false;
+// }
+// bool isSorted(vector<int> a) {
+//     int c = 0;
+//     for (int i = 0; i < a.size() - 1; i++)
+//     {
+//         if (!(a[i] <= a[i + 1]))
+//         {
+//             c++;
+//         }
+//     }
+
+//     if (c > 1)
+//     {
+//         return false;
+//     }
+//     return true;
+// }
+
+bool check(vector<int>& a) {
+    int i = 0;
+    while (i < a.size())
     {
         vector<int> temp;
-        temp = rotate(nums, i);
-        if (isSorted(temp))
+        temp = rotate(a, i);
+        if (!isSorted(temp))
         {
+            i++;
+        }
+        else {
             return true;
         }
-        
+
     }
     return false;
 }
 
+
 int main()
 {
-    vector<int> v = {4,3,5,1,2};
-    bool k = check(v);
+    vector<int> v = {3,4,1,5,2};
+    bool k = isSorted(v);
     cout << k << endl;
     return 0;
 }
