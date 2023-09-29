adecbbce61baae3698a6682d309ff1f73e233a2f
ishant9805 <ishant9805@gmail.com>
2023-09-29 16:06:09 +0530

    Solved with n^2 time complexity

--- codingNinja/removeDuplicates.cpp
+++ codingNinja/removeDuplicates.cpp
@@ -2,33 +2,33 @@
 #include<vector>
 using namespace std;
 
-// bool isPresent(vector<int> &is, int x) {
-//     for (int i = 0; i < is.size(); i++)
-//     {
-//         if (is[i] == x)
-//         {
-//             return true;
-//         }
-//     }
-//     return false;
-// }
+bool isPresent(vector<int> &is, int x) {
+    for (int i = 0; i < is.size(); i++)
+    {
+        if (is[i] == x)
+        {
+            return true;
+        }
+    }
+    return false;
+}
 
-// int removeDuplicates(vector<int> &arr, int n) {
-// 	// Write your code here.
-//     int ss = 0;
-//     vector<int> is;
-//     for (int i = 0; i < n; i++)
-//     {
-//         if (isPresent(is, arr[i]))
-//         {
-//             continue;
-//         }
-//         else {
-//             is.push_back(arr[i]);
-//         }
-//     }
-//     return is.size();
-// }
+int removeDuplicates(vector<int> &arr, int n) {
+	// Write your code here.
+    int ss = 0;
+    vector<int> is;
+    for (int i = 0; i < n; i++)
+    {
+        if (isPresent(is, arr[i]))
+        {
+            continue;
+        }
+        else {
+            is.push_back(arr[i]);
+        }
+    }
+    return is.size();
+}
 
 int main()
 {
