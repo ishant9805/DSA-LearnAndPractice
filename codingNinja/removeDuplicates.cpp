09b35d613bb76d8dbf51101843c8c6ea65ecd77c
ishant9805 <ishant9805@gmail.com>
2023-09-29 16:24:46 +0530

    Finally! did it in O(n). 🎉🥳

--- codingNinja/removeDuplicates.cpp
+++ codingNinja/removeDuplicates.cpp
@@ -2,37 +2,50 @@
 #include<vector>
 using namespace std;
 
-bool isPresent(vector<int> &is, int x) {
-    for (int i = 0; i < is.size(); i++)
-    {
-        if (is[i] == x)
-        {
-            return true;
-        }
-    }
-    return false;
-}
+// bool isPresent(vector<int> &is, int x) {
+//     for (int i = 0; i < is.size(); i++)
+//     {
+//         if (is[i] == x)
+//         {
+//             return true;
+//         }
+//     }
+//     return false;
+// }
+
+// int removeDuplicates(vector<int> &arr, int n) {
+// 	// Write your code here.
+//     int ss = 0;
+//     vector<int> is;
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (isPresent(is, arr[i]))
+//         {
+//             continue;
+//         }
+//         else {
+//             is.push_back(arr[i]);
+//         }
+//     }
+//     return is.size();
+// }
 
 int removeDuplicates(vector<int> &arr, int n) {
-	// Write your code here.
-    int ss = 0;
-    vector<int> is;
-    for (int i = 0; i < n; i++)
-    {
-        if (isPresent(is, arr[i]))
-        {
-            continue;
+    int ss = 1;
+    for (int i = 0; i < arr.size() - 1; i++) {
+        if (!(arr[i] == arr[i + 1])) {
+            ss++;
         }
         else {
-            is.push_back(arr[i]);
+            continue;
         }
     }
-    return is.size();
+    return ss;
 }
 
 int main()
 {
-    vector<int> v = {1,3,1,4,2};
+    vector<int> v = {1, 1, 2, 2, 3, 3, 3, 4, 4, 5, 5};
     int k = removeDuplicates(v, v.size());
     cout << k << endl;
     return 0;
