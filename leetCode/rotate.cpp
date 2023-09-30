1c13dd90f5fd4bf8a22142397e1f1e45a31710e4
ishant9805 <ishant9805@gmail.com>
2023-09-30 22:08:21 +0530

    solved to an extent

--- leetCode/rotate.cpp
+++ leetCode/rotate.cpp
@@ -2,28 +2,41 @@
 #include<vector>
 using namespace std;
 
-vector<int> rotateArray(vector<int> &nums, int n) {
-    int tmp = nums[0];
-    for (int i = 0; i < n - 1; i++)
-    {
-        nums[i] = nums[i + 1];
-    }
-    nums[n - 1] = tmp;
-    return nums;
-}
+// vector<int> rotateArray(vector<int> &nums, int n) {
+//     int tmp = nums[0];
+//     for (int i = 0; i < n - 1; i++)
+//     {
+//         nums[i] = nums[i + 1];
+//     }
+//     nums[n - 1] = tmp;
+//     return nums;
+// }
 
 void rotate(vector<int>& nums, int k) {
     int n = nums.size();
-    for (int i = 0; i <= k; i++)
+    if (n == 1)
+        return;
+    vector<int> tmp;
+    int l = n - k;
+    for (int i = l; i < n; i++)
     {
-        nums = rotateArray(nums, n);
+        tmp.push_back(nums[i]);
+    }
+    
+    for (int i = 0; i < l; i++)
+    {
+        tmp.push_back(nums[i]);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        nums[i] = tmp[i];
     }
 }
 
 int main()
 {
-    vector<int> v = {1,2,3,4,5,6,7};
-    rotate(v, 3);
+    vector<int> v = {-1}; // {}
+    rotate(v, 2);
     cout << "[";
     for (int i = 0; i < v.size() - 1; i++)
     {
