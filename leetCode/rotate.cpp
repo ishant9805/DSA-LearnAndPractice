292db21ed43136795ec06a0bf48ba813467befde
ishant9805 <ishant9805@gmail.com>
2023-10-01 13:37:08 +0530

    Needed to see solution

--- leetCode/rotate.cpp
+++ leetCode/rotate.cpp
@@ -2,6 +2,13 @@
 #include<vector>
 using namespace std;
 
+// https://leetcode.com/problems/rotate-array/description/
+// E.g. array = {1,2,3,4,5,6,7} and k = 3
+// Reverse the array till kth part => array = {4,3,2,1,5,6,7}
+// Reverse the array after kth part => array = {4,3,2,1,7,6,5}
+// Reverse the whole array => array = {5,6,7,1,2,3,4}
+// It's not my solution, I couldn't solve it 😥
+
 void rotate(vector<int>& nums, int k) {
     k = k % nums.size();
     reverse(nums.begin(), nums.begin() + (nums.size() - k));
