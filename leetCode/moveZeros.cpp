0641f8bca7a3c6316d24182f73ba33e1b46cd003
ishant9805 <ishant9805@gmail.com>
2023-10-02 20:26:34 +0530

    Already solved but making optimal

---
+++ leetCode/moveZeros.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void moveZeroes(vector<int>& nums) {
+    int n = nums.size();
+    int k = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (nums[i] == 0)
+        {
+            nums.erase(nums.begin() + i);
+            i--;
+        }
+    }
+}
+
+int main()
+{
+    vector<int> v = {0,1,0,3,12};
+    moveZeroes(v);
+    cout << "[";
+    for (int i = 0; i < v.size() - 1; i++)
+    {
+       cout << v[i] << ", ";
+    }
+    cout << v[v.size() - 1] << "]" << endl;
+    return 0;
+}
