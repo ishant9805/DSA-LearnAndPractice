6e4bd3f4a84a3d2b1325c15a5094dcf4ff649cc9
ishant9805 <ishant9805@gmail.com>
2023-10-02 20:44:54 +0530

    Done ✅

--- leetCode/moveZeros.cpp
+++ leetCode/moveZeros.cpp
@@ -2,24 +2,36 @@
 #include<vector>
 using namespace std;
 
+// Problem Link: https://leetcode.com/problems/move-zeroes/description/
+
+// My Own Solution
+// void moveZeroes(vector<int>& nums) {
+//     int n = nums.size();
+//     int k = 0;
+//     for (int i = n - 1; i >= 0; i--)
+//     {
+//         if (nums[i] == 0)
+//         {
+//             nums.erase(nums.begin() + i);
+//             k++;
+//         }
+//     }
+//     for (int i = 0; i < k; i++)
+//     {
+//         nums.push_back(0);
+//     }
+// }
+
+// Found Optimal solution Thanks to https://leetcode.com/freshfreshman/
 void moveZeroes(vector<int>& nums) {
-    int n = nums.size();
-    int k = 0;
-    for (int i = 0; i < n; i++)
-    {
-        if (nums[i] == 0)
-        {
-            nums.erase(nums.begin() + i);
-            i--;
-            k++;
-            n--;
+    int i=0,j=0;
+    while(j<nums.size()){
+        if(nums[j]!=0){
+            swap(nums[i],nums[j]);
+            i++;
         }
+        j++;
     }
-    for (int i = 0; i < k; i++)
-    {
-        nums.push_back(0);
-    }
-    
 }
 
 int main()
