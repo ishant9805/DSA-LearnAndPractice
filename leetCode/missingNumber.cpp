09d0791bfd7063b77a104ed242db4490179b1a50
ishant9805 <ishant9805@gmail.com>
2023-10-07 20:08:42 +0530

    Solved it in required time and space complexity.

--- leetCode/missingNumber.cpp
+++ leetCode/missingNumber.cpp
@@ -1,14 +1,23 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 
 int missingNumber(vector<int>& nums) {
-    
+    sort(nums.begin(), nums.end());
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (nums[i] != i)
+        {
+            return i;
+        }
+    }
+    return nums.size();
 }
 
 int main()
 {
-    vector<int> nums = {3,0,1};
+    vector<int> nums = {0,1};
     int n = missingNumber(nums);
     cout << n << endl;
     return 0;
