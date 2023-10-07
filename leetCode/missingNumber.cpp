ec8569cd34dfafdcf4b743f2cbd3870ad7fbf5fc
ishant9805 <ishant9805@gmail.com>
2023-10-07 19:44:02 +0530

    Started leetcode problem

---
+++ leetCode/missingNumber.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int missingNumber(vector<int>& nums) {
+    
+}
+
+int main()
+{
+    vector<int> nums = {3,0,1};
+    int n = missingNumber(nums);
+    cout << n << endl;
+    return 0;
+}
