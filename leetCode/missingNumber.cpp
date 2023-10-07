b406787833e4135aed44ff51681b57c2970c6a55
ishant9805 <ishant9805@gmail.com>
2023-10-07 20:18:41 +0530

    A nice approach

--- leetCode/missingNumber.cpp
+++ leetCode/missingNumber.cpp
@@ -1,18 +1,26 @@
 #include<iostream>
 #include<vector>
-#include<algorithm>
+#include<numeric>
 using namespace std;
 
-int missingNumber(vector<int>& nums) {
-    sort(nums.begin(), nums.end());
-    for (int i = 0; i < nums.size(); i++)
-    {
-        if (nums[i] != i)
-        {
-            return i;
-        }
-    }
-    return nums.size();
+// int missingNumber(vector<int>& nums) {
+//     sort(nums.begin(), nums.end());
+//     for (int i = 0; i < nums.size(); i++)
+//     {
+//         if (nums[i] != i)
+//         {
+//             return i;
+//         }
+//     }
+//     return nums.size();
+// }
+
+// Had an out of the box thought
+int missingNumber(vector<int> v) {
+    int vS = v.size();
+    int expectedSum = vS * (vS + 1) / 2;
+    int actualSum = accumulate(v.begin(), v.end(), 0);
+    return expectedSum - actualSum;
 }
 
 int main()
