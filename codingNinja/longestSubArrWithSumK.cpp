ac02ab0fba54d7393658cd4c7e4252b1a12c63f7
ishant9805 <ishant9805@gmail.com>
2023-10-12 19:36:40 +0530

    Solved but got 10/11

--- codingNinja/longestSubArrWithSumK.cpp
+++ codingNinja/longestSubArrWithSumK.cpp
@@ -2,38 +2,60 @@
 #include<vector>
 using namespace std;
 
+// int longestSubarrayWithSumK(vector<int> a, long long k) {
+//     // Write your code here
+//     int mx = INT_MIN;
+//     // vector<int> tmp;
+//     int sum = 0;
+//     for (int i = 0; i < a.size(); i++)
+//     {
+//         for (int j = i; j < a.size(); j++)
+//         {
+//             sum = 0;
+//             for (int k = i; k <= j; k++)
+//             {
+//                 sum += a[k];
+//             }
+//             if (sum == k)
+//             {
+//                 mx = max(mx, j - i + 1);
+//             }
+            
+//         }
+        
+//     }
+//     return mx;
+// }
+
+// Trying other method
 int longestSubarrayWithSumK(vector<int> a, long long k) {
-    // Write your code here
-    int mx = INT_MIN;
-    // vector<int> tmp;
-    int sum = 0;
+    int wholeSum = 0;
+    int partSum = 0;
+    int mx = 0;
     for (int i = 0; i < a.size(); i++)
     {
-        for (int j = i; j < a.size(); j++)
+        partSum = 0;
+        wholeSum += a[i];
+        cout << "i: " << i << " Whole Sum: " << wholeSum << " Part Sum: [";
+        for (int j = 0; j <= i; j++)
         {
-            sum = 0;
-            // cout << "[";
-            for (int k = i; k <= j; k++)
+            partSum += a[j];
+            cout << partSum << ",";
+            if (wholeSum - partSum == k)
             {
-                sum += a[k];
-                // cout << " " << a[k] << ",";
+                mx = max(mx, i - j);
             }
-            // cout << "]" << endl;
-            if (sum == k)
-            {
-                mx = max(mx, j - i + 1);
-            }
-            
         }
-        
+        cout << "]" << endl;
     }
     return mx;
 }
 
 int main()
 {
-    vector<int> v = {1,2,3,1,1,1,1};
-    int a = longestSubarrayWithSumK(v, 3);
+    // vector<int> v = {1,2,3,1,1,1,1};
+    vector<int> v = {1,2,1,3};
+    int a = longestSubarrayWithSumK(v, 2);
     cout << a << endl;
     return 0;
 }
