2f64dc2156ed3380e237253d9aa860bfc10bf80e
ishant9805 <ishant9805@gmail.com>
2023-10-15 20:26:03 +0530

    solved using two-pointers ✅

--- codingNinja/twoSums.cpp
+++ codingNinja/twoSums.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 #include<map>
+#include<algorithm>
 using namespace std;
 
 // Loop method
@@ -25,25 +26,46 @@ using namespace std;
 // This function takes three input (size, arr, target) and returns YES or NO, 
 // if target exist as sum of two elements in given arr.
 // E.g. string result = read(arr_size:int, arr:vector<int>, target:int)
+// string read(int n, vector<int> book, int target)
+// {
+//     map<int, int> mp; int diff = 0;
+//     for (int i = 0; i < n; i++)
+//     {
+//         diff = target - book[i];
+//         if (mp.find(diff) != mp.end())
+//         {
+//             return "YES";
+//         }
+//         mp.insert({book[i], i});        
+//     }
+//     return "NO";
+// }
+
+// Two pointer solution
 string read(int n, vector<int> book, int target)
 {
-    map<int, int> mp; int diff = 0;
-    for (int i = 0; i < n; i++)
+    sort(book.begin(), book.begin() + n);
+    cout << "[";
+    for (int i = 0; i < book.size() - 1; i++)
     {
-        diff = target - book[i];
-        if (mp.find(diff) != mp.end())
-        {
-            return "YES";
-        }
-        mp.insert({book[i], i});        
+       cout << book[i] << ", ";
+    }
+    cout << book[book.size() - 1] << "]" << endl;
+    int left = 0; int right = n - 1; int sum = 0;
+    while (left < right)
+    {
+        sum = book[left] + book[right];
+        if (sum == target) return "YES";
+        if (sum > target) right--;
+        if (sum < target) left++;
     }
     return "NO";
 }
 
 int main()
 {
-    vector<int> book = {1,2};
-    string ans = read(book.size(), book, 2);
+    vector<int> book = {2,8,7,7,5,3,4};
+    string ans = read(book.size(), book, 9);
     cout << ans << endl;
     return 0;
 }
