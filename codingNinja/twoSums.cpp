6fbbb332032ff32565931f1270b324f0041bc275
ishant9805 <ishant9805@gmail.com>
2023-10-15 19:45:57 +0530

    solved ✅

--- codingNinja/twoSums.cpp
+++ codingNinja/twoSums.cpp
@@ -1,10 +1,29 @@
 #include<iostream>
-
+#include<vector>
 using namespace std;
+
+string read(int n, vector<int> book, int target)
+{
+    int left = 0; int right = 0; int sum = 0;
+    while (left < n - 1) {
+        right = left + 1;
+        while (right < n)
+        {
+            sum = book[left] + book[right];
+            if (sum == target)
+                return "Yes";
+            right++;
+        }
+        left++;
+    }
+    
+    return "No";
+}
+
 int main()
 {
-    int n;
-    cin >> n;
-    /*CodeHere*/
+    vector<int> book = {1,2};
+    string ans = read(book.size(), book, 2);
+    cout << ans << endl;
     return 0;
 }
