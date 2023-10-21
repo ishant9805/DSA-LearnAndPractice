a50d49e9659a7543418364078aaeead858a26192
ishant9805 <ishant9805@gmail.com>
2023-10-22 00:31:45 +0530

    Solved ✅ using kadane's algo

--- leetCode/maxSubarraySum.cpp
+++ leetCode/maxSubarraySum.cpp
@@ -3,25 +3,24 @@
 using namespace std;
 
 // Kadane's Algo
-long long maxSubarraySum(vector<int> arr, int n)
+int maxSubarraySum(vector<int> arr)
 {
     // Write your code here.
-    long long max_sum = arr[0];
+    int max_sum = arr[0];
     int sum = 0;
-    for (int a = 0; a < n; a++)
+    for (int a = 0; a < arr.size(); a++)
     {
         sum += arr[a];
         if (sum > max_sum) max_sum = sum;
         if (sum < 0) sum = 0;
     }
-    if (max_sum <= 0) return 0;
     return max_sum;
 }
 
 int main()
 {
     vector<int> v = {-7, -8, -16, -4, -8, -5, -7, -11, -10, -12, -4, -6, -4, -16, -10};
-    long long it = maxSubarraySum(v, v.size());
+    long long it = maxSubarraySum(v);
     cout << it << endl;
     return 0;
 }
