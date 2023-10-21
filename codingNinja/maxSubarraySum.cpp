b5ee2a9f59c05ea2c9dde8ddc4f10e462b4f9d0d
ishant9805 <ishant9805@gmail.com>
2023-10-22 00:27:26 +0530

    Solved ✅ using kadane's algo

--- codingNinja/maxSubarraySum.cpp
+++ codingNinja/maxSubarraySum.cpp
@@ -2,19 +2,25 @@
 #include<vector>
 using namespace std;
 
+// Kadane's Algo
 long long maxSubarraySum(vector<int> arr, int n)
 {
     // Write your code here.
-    for (int i = 0; i < n; i++)
+    long long max_sum = arr[0];
+    int sum = 0;
+    for (int a = 0; a < n; a++)
     {
-        
+        sum += arr[a];
+        if (sum > max_sum) max_sum = sum;
+        if (sum < 0) sum = 0;
     }
-    
+    if (max_sum <= 0) return 0;
+    return max_sum;
 }
 
 int main()
 {
-    vector<int> v = {1,2,7,-4,3,2,-10,9,1};
+    vector<int> v = {-7, -8, -16, -4, -8, -5, -7, -11, -10, -12, -4, -6, -4, -16, -10};
     long long it = maxSubarraySum(v, v.size());
     cout << it << endl;
     return 0;
