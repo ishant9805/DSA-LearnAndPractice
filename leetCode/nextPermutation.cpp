892bc06a20c1b0943a4a91f106ca0d4c437a61d3
ishant9805 <ishant9805@gmail.com>
2023-10-24 21:48:53 +0530

    Solved using premade lib

---
+++ leetCode/nextPermutation.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+vector<int> nextPermutation(vector<int> &A) {
+    next_permutation(A.begin(), A.end());
+    return A;
+}
+
+int main()
+{
+    vector<int> v = {1,3,2};
+    v = nextPermutation(v);
+    cout << "[";
+    for (int i = 0; i < v.size() - 1; i++)
+    {
+       cout << v[i] << ", ";
+    }
+    cout << v[v.size() - 1] << "]" << endl;
+    return 0;
+}
