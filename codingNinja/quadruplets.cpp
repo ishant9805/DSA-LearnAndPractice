7660c88d3befd48925dbd089cc1c187b28c610c6
ishant9805 <ishant9805@gmail.com>
2023-11-14 23:52:55 +0530

    Solved with help of tutorial with two pointer approach

--- codingNinja/quadruplets.cpp
+++ codingNinja/quadruplets.cpp
@@ -4,31 +4,65 @@
 #include<set>
 
 using namespace std;
+
+// Better Approach
+// vector<vector<int>> fourSum(vector<int>& nums, int target) {
+//     int sz = nums.size();
+//     set<vector<int>> res;
+//     for (int i = 0; i < sz; i++)
+//     {
+//         for (int j = i + 1; j < sz; j++)
+//         {
+//             set<long long> hashset;
+//             for (int k = j + 1; k < sz; k++)
+//             {
+//                 long long sum = nums[i] + nums[j];
+//                 sum += nums[k];
+//                 long long fourth = target - (sum);
+//                 if (hashset.find(fourth) != hashset.end())
+//                 {
+//                     vector<int> tmp = {nums[i], nums[j], nums[k], (int) fourth};
+//                     sort(tmp.begin(), tmp.end());
+//                     res.insert(tmp);
+//                 }
+//                 hashset.insert(nums[k]);
+//             }
+//         }
+//     }
+//     vector<vector<int>> v(res.begin(), res.end());
+//     return v;
+// }
+
+// Two Pointer Approach
 vector<vector<int>> fourSum(vector<int>& nums, int target) {
     int sz = nums.size();
-    set<vector<int>> res;
+    sort(nums.begin(), nums.end());
+    vector<vector<int>> res;
     for (int i = 0; i < sz; i++)
     {
+        if(i > 0 && nums[i] == nums[i-1]) continue;
         for (int j = i + 1; j < sz; j++)
         {
-            set<long long> hashset;
-            for (int k = j + 1; k < sz; k++)
+            if(j > i + 1 && nums[j] == nums[j-1]) continue;
+            int k = j + 1, l = sz - 1;
+            while (k < l)
             {
-                long long sum = nums[i] + nums[j];
-                sum += nums[k];
-                long long fourth = target - (sum);
-                if (hashset.find(fourth) != hashset.end())
-                {
-                    vector<int> tmp = {nums[i], nums[j], nums[k], (int) fourth};
-                    sort(tmp.begin(), tmp.end());
-                    res.insert(tmp);
+                long long sum = nums[i] + nums[j] + nums[k] + nums[l];
+                if (sum > target) l--;
+                else if (sum < target) k++;
+                else {
+                    res.push_back({nums[i], nums[j], nums[k], nums[l]});
+                    k++, l--;
+                    while(nums[k] == nums[k - 1]) k++;
+                    while(nums[l] == nums[l + 1]) l--;
                 }
-                hashset.insert(nums[k]);
+                
             }
+            
         }
+        
     }
-    vector<vector<int>> v(res.begin(), res.end());
-    return v;
+    return res;
 }
 
 int main()
