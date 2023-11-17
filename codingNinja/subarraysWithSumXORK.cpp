caf132868e2a42a7b9ba08233a9b28c0e514a9a3
ishant9805 <ishant9805@gmail.com>
2023-11-17 23:46:56 +0530

    Solved

--- codingNinja/subarraysWithSumXORK.cpp
+++ codingNinja/subarraysWithSumXORK.cpp
@@ -1,26 +1,44 @@
 #include<iostream>
 #include<vector>
+#include<unordered_map>
 
 using namespace std;
 
 // Brrute Force Approach
-int subarraysWithSumK(vector < int > a, int b) {
-    int cnt = 0, sz = a.size();
+// int subarraysWithSumK(vector < int > a, int b) {
+//     int cnt = 0, sz = a.size();
+//     for (int i = 0; i < sz; i++)
+//     {
+//         for (int j = i; j < sz; j++)
+//         {
+//             int xum = 0;
+//             for (int k = i; k <= j; k++)
+//             {
+//                 xum = xum ^ a[k];
+//             }
+//             if (xum == b)
+//             {
+//                 cnt += 1;
+//             }
+//         }
+        
+//     }
+//     return cnt;
+// }
+
+// Optimal Approach
+int subarraysWithSumK(vector < int > vec, int target) {
+    unordered_map<int, int> umpp;
+    umpp[0] = 1;
+    int preSum = 0;
+    int cnt = 0;
+    int sz = vec.size();
     for (int i = 0; i < sz; i++)
     {
-        for (int j = i; j < sz; j++)
-        {
-            int xum = 0;
-            for (int k = i; k <= j; k++)
-            {
-                xum = xum ^ a[k];
-            }
-            if (xum == b)
-            {
-                cnt += 1;
-            }
-        }
-        
+        preSum ^= vec[i];
+        int rmv = preSum ^ target;
+        cnt += umpp[rmv];
+        umpp[preSum] += 1;
     }
     return cnt;
 }
