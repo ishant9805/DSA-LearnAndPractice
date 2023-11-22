cb46b57a861a1bbc4c3328b568ca35871a8eb551
ishant9805 <ishant9805@gmail.com>
2023-11-22 21:29:03 +0530

    Solved on my own

--- codingNinja/missingRepeatingNums.cpp
+++ codingNinja/missingRepeatingNums.cpp
@@ -5,17 +5,29 @@
 using namespace std;
 
 vector<int> findMissingRepeatingNumbers(vector < int > a) {
+    auto bg = a.begin();
     int sz = a.size();
-    int tot = (sz * (sz - 1)) / 2;
-    int actSum = accumulate(a.begin(), a.end(), 0);
-    int ab = actSum - tot;
-    // sz = 4, tot = 6, actSum = 9, ab = 3
-    return {ab, ab};
+    sort(bg, bg+sz); //O(nlogn)
+    vector<int> ans;
+    int actSum = accumulate(bg, bg+sz, 0);
+    int totSum = ((sz) * (sz + 1))/2;
+    for (int i = 0; i < sz - 1; i++)
+    {
+        if (a[i + 1] == a[i])
+        {
+            ans.push_back(a[i]);
+            ans.push_back(totSum-(actSum-a[i]));
+            break;
+        }
+        
+    }
+    
+    return ans;
 }
 
 int main()
 {
-    vector<int> v = {1, 2, 3, 2};
+    vector<int> v = {4,3,6,2,1,1};
     v = findMissingRepeatingNumbers(v);
     cout << "[";
     for (int i = 0; i < v.size() - 1; i++)
