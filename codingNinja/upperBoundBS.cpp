695839eb4c96e45f967834f96d061c2e31155828
ishant9805 <ishant9805@gmail.com>
2023-12-02 18:14:29 +0530

    Solved with help of tutorial

---
+++ codingNinja/upperBoundBS.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int upperBound(vector<int> &arr, int x, int n){
+	int low = 0; // 1 2 5 6 10
+    int high = n - 1;
+    int mid;
+    int ans = n;
+    while (low <= high)
+    {
+        mid = (high + low) / 2;
+        if (arr[mid] > x) {
+            ans = mid;
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<int> v = {1,2,5,6,10};
+    int k = upperBound(v, 10, v.size()); // 5
+    cout << k << endl;
+    v = {1,4,7,8,10};
+    k = upperBound(v, 7, v.size()); // 3
+    cout << k << endl;
+    v = {1,5,5,7,7,9,10};
+    k = upperBound(v, 5, v.size()); // 3
+    cout << k << endl;
+    v = {5, 12, 12, 15, 18, 21, 35, 37, 38, 46, 47, 48, 48, 50};
+    k = upperBound(v, 45, v.size()); // 3
+    cout << k << endl;
+    v = {2, 5, 6, 19, 24, 25, 26, 26, 32, 42, 44};
+    k = upperBound(v, 42, v.size()); // 3
+    cout << k << endl;
+    
+    return 0;
+}
