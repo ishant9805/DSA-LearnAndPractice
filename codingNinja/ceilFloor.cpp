e8a38dd6e1d94321df9bf5c779c95570b38dbd88
ishant9805 <ishant9805@gmail.com>
2023-12-04 19:10:32 +0530

    Solved with time complexity of O(logn) by myself �

--- codingNinja/ceilFloor.cpp
+++ codingNinja/ceilFloor.cpp
@@ -4,26 +4,49 @@
 using namespace std;
 
 // Brute Force approach
+// pair<int, int> getFloorAndCeil(vector<int> &a, int n, int x) {
+// 	// Write your code here.
+//     int ceil = -1;
+//     int flr = -1;
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (a[i] >= x)
+//         {
+//             ceil = a[i];
+//             break;
+//         }        
+//     }
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (a[i] <= x)
+//         {
+//             flr = a[i];
+//         }        
+//     }
+//     return {flr, ceil};
+// }
+
+// Optimal approach
 pair<int, int> getFloorAndCeil(vector<int> &a, int n, int x) {
-	// Write your code here.
-    int ceil = -1;
-    int flr = -1;
-    for (int i = 0; i < n; i++)
+	pair<int, int> ans = {-1 , -1};
+    int low = 0, high = n - 1;
+    while (low <= high)
     {
-        if (a[i] >= x)
+        int mid = (low + high) / 2;
+        if (a[mid] == x)
         {
-            ceil = a[i];
-            break;
-        }        
+            ans.first = a[mid];
+            ans.second = a[mid];
+            return ans;
+        } else if (a[mid] < x) {
+            low = mid + 1;
+            ans.first = a[mid];
+        } else {
+            high = mid - 1;
+            ans.second = a[mid];
+        }
     }
-    for (int i = 0; i < n; i++)
-    {
-        if (a[i] <= x)
-        {
-            flr = a[i];
-        }        
-    }
-    return {flr, ceil};
+    return ans;
 }
 
 int main()
@@ -32,6 +55,9 @@ int main()
     pair<int, int> k = getFloorAndCeil(v, v.size(), 5);
     cout << "pair<" << k.first << ", " << k.second << ">" << endl;
     k = getFloorAndCeil(v, v.size(), 8);
+    cout << "pair<" << k.first << ", " << k.second << ">" << endl;
+    v = {2};
+    k = getFloorAndCeil(v, v.size(), 23);
     cout << "pair<" << k.first << ", " << k.second << ">" << endl;
     return 0;
 }
