f98962610324236d04b0f429e98b58a45901b7c2
ishant9805 <ishant9805@gmail.com>
2023-12-08 19:21:24 +0530

    started solving occurence counter

---
+++ codingNinja/numOfOccurs.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int count(vector<int>& arr, int n, int x) {
+	int low = 0, high = n - 1;
+    while (low < high)
+    {
+        
+    }
+    
+}
+
+int main()
+{
+    vector<int> v = {1,1,1,2,2,3,3};
+    int k = count(v, v.size(), 3);
+    cout << k << endl;
+    return 0;
+}
