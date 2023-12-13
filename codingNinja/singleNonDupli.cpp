e2c812918b0d0ff06ebd65a8b448ee9192d77a6b
ishant9805 <ishant9805@gmail.com>
2023-12-13 19:35:49 +0530

    used binary search to improve time complexity but didn't work

--- codingNinja/singleNonDupli.cpp
+++ codingNinja/singleNonDupli.cpp
@@ -4,13 +4,30 @@
 using namespace std;
 
 // Brute Force
+// int singleNonDuplicate(vector<int>& arr) {
+//     for (int i = 0; i < arr.size(); i++)
+//     {
+//         if (arr[i - 1] == arr[i] || arr[i] == arr[i + 1]) continue;
+//         else return arr[i];
+//     }
+//     return arr[0];   
+// }
+
+// Binary Search
 int singleNonDuplicate(vector<int>& arr) {
-    for (int i = 0; i < arr.size(); i++)
+	int sz = arr.size();
+    if (sz == 1) return arr[0];
+    if (arr[0] != arr[1]) return arr[0];
+    if (arr[sz - 2] != arr[sz - 1]) return arr[sz - 1];
+    int low = 1, high = sz - 2;
+    while (low <= high)
     {
-        if (arr[i - 1] == arr[i] || arr[i] == arr[i + 1]) continue;
-        else return arr[i];
+        int mid = (low + high) / 2;
+        if (arr[mid] != arr[mid - 1] && arr[mid] != arr[mid + 1]) return arr[mid];
+        if (mid % 2 == 1 && arr[mid - 1] == arr[mid] || mid % 2 == 0 && arr[mid + 1] == arr[mid]) low = mid + 1;
+        else high = mid - 1;
     }
-    return arr[0];   
+    return arr[sz - 1];
 }
 
 int main()
--- leetCode/singleNonDupli.cpp
+++ leetCode/singleNonDupli.cpp
@@ -19,6 +19,32 @@ int singleNonDuplicate(vector<int>& arr) {
     return arr[arr.size() - 1];   
 }
 
+int singleNonDuplicate(vector<int>& arr) {
+	int sz = arr.size();
+    // If arr contains only one element, obviously it would be the single one
+    if (sz == 1) return arr[0];
+    // if 0th != 1st element of arr, then 0th will be single as arr is sorted
+    if (arr[0] != arr[1]) return arr[0];
+    // same will happen to last element
+    if (arr[sz - 2] != arr[sz - 1]) return arr[sz - 1];
+    // eliminate the first and last for safety
+    int low = 1, high = sz - 2;
+    // Applying BS
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        // If found
+        if (arr[mid] != arr[mid - 1] && arr[mid] != arr[mid + 1]) return arr[mid];
+        // Either if we are at odd and left is equal or we are at even and right is equal
+        // then it means element is in right of mid and we need to eliminate left part
+        if (mid % 2 == 1 && arr[mid - 1] == arr[mid] || mid % 2 == 0 && arr[mid + 1] == arr[mid]) low = mid + 1;
+        // otherwise
+        else high = mid - 1;
+    }
+    // For safety
+    return arr[sz - 1];
+}
+
 int main()
 {
     vector<int> v = {1,1,4,4,15};
