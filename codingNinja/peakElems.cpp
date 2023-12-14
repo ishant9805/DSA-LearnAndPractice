1e7b1d8e72c386d4fa002422b1bdaf1cfbca4373
ishant9805 <ishant9805@gmail.com>
2023-12-14 19:35:59 +0530

    Solved on my own

---
+++ codingNinja/peakElems.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+// Brute Force
+int findPeakElement(vector<int> &arr) {
+    int n = arr.size();
+    if (arr[0] > arr[1]) return 0;
+    if (arr[n-1] > arr[n-2]) return n-1;
+    for (int i = 1; i < n - 2; i++)
+    {
+        if (arr[i - 1] < arr[i] && arr[i] > arr[i + 1])
+        {
+            return i;
+        }
+        
+    }
+    return -1;
+}
+
+int main()
+{
+    vector<int> v = {1,8,1,5,3};
+    cout << findPeakElement(v) << endl;
+    return 0;
+}
