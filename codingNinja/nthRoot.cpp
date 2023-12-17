ea1dad01d68c52711592dc3aeec45b40e10c2e6f
ishant9805 <ishant9805@gmail.com>
2023-12-17 13:41:21 +0530

    Solved

--- codingNinja/nthRoot.cpp
+++ codingNinja/nthRoot.cpp
@@ -4,15 +4,39 @@
 using namespace std;
 
 // Linear checks
+// int NthRoot(int n, int m) {
+//     for (int i = 0; i < m; i++)
+//     {
+//         long long val = pow(i, n);
+//         if (val == m) {
+//             return i;
+//         } else if (val > m) {
+//             break;
+//         }
+//     }
+//     return -1;
+// }
+
+// Binary search
+int powe(int mid, int n, int m) {
+    long long ans = 1;
+    for (int i = 1; i <= n; i++) {
+        ans = ans * mid;
+        if (ans > m) return 0;
+    }
+    if (ans == m) return 1;
+    return 2;
+}
+
 int NthRoot(int n, int m) {
-    for (int i = 0; i < m; i++)
+    int l = 1, h = m;
+    while (l <= h)
     {
-        long long val = pow(i, n);
-        if (val == m) {
-            return i;
-        } else if (val > m) {
-            break;
-        }
+        int mid = (l + h) / 2;
+        int res = powe(mid, n, m);
+        if (res == 1) return mid;
+        else if (res == 0) h = mid - 1;
+        else l = mid + 1;
     }
     return -1;
 }
