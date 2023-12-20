33492f633835ec9713467f0cfabb6aedccea2cf1
ishant9805 <ishant9805@gmail.com>
2023-12-20 19:55:53 +0530

    Solved with help of tutorial

--- codingNinja/kokoBananas.cpp
+++ codingNinja/kokoBananas.cpp
@@ -1,28 +1,41 @@
 #include<iostream>
 #include<vector>
 #include<numeric>
+#include<xutility>
 
 using namespace std;
+// using binary search
+int possibility(vector<int> &v, int rate) {
+    int hrs = 0;
+    for (int i = 0; i < v.size(); i++)
+    {
+        hrs += ceil((double)(v[i]) / (double) (rate));
+    }
+    
+    return hrs;
+}
+
+int maxEle(vector<int> &v) {
+    int maxi = INT_MIN;
+    for (int i = 0; i < v.size(); i++)
+    {
+        maxi = max(maxi, v[i]);
+    }
+    return maxi;
+}
 
 int minimumRateToEatBananas(vector<int> v, int h) {
-    for (int i = 3; i <= h; i++) {
-        vector<int> vTmp(v.size());
-        for (int j = 0; j < v.size(); j++)
-        {
-            vTmp[j] = ceil(v[j] / (float) i);
-        }
-        int sum = accumulate(vTmp.begin(), vTmp.end(), 0);
-        int hrs = 0;
-        while (sum > 0) {
-            sum -= i;
-            hrs++;
-            if (hrs > h) break;
-        }
-        if(hrs == h) {
-            return i;
+    int high = maxEle(v);
+    int low = 1;
+    while(low <= high) {
+        int mid = (low + high) / 2;
+        if (possibility(v, mid) <= h) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
         }
     }
-    return -1;
+    return low;
 }
 
 int main()
