d9993c8dffa8fe0236e8e53db85a17179e551c30
ishant9805 <ishant9805@gmail.com>
2023-12-27 19:34:57 +0530

    Solved with help of article

--- codingNinja/missingK.cpp
+++ codingNinja/missingK.cpp
@@ -3,36 +3,55 @@
 
 using namespace std;
 
-int findNum(vector<int> v, int l) {
-    int low = 0, high = v.size() - 1;
-    while (low <= high)
-    {
+// Brute Force
+// int findNum(vector<int> v, int l) {
+//     int low = 0, high = v.size() - 1;
+//     while (low <= high)
+//     {
+//         int mid = (low + high) / 2;
+//         if (v[mid] == l) return mid;
+//         else if (v[mid] < l) {
+//             low = mid + 1;
+//         } else {
+//             high = mid - 1;
+//         }
+//     }
+//     return -1;
+// }
+// int missingK(vector < int > vec, int n, int k) {
+//     int cnt = 0;
+//     int maxi = vec[n - 1];
+//     for (int i = 1; i < maxi; i++)
+//     {
+//         if (findNum(vec, i) == -1)
+//         {
+//             cnt++;
+//         }
+//         if (cnt == k)
+//         {
+//             return i;
+//         }   
+//     }
+//     return maxi+(k-cnt);
+// }
+
+int missingK(vector<int> v, int n, int k) {
+    int low = 0, high = n - 1;
+    while (low <=  high) {
         int mid = (low + high) / 2;
-        if (v[mid] == l) return mid;
-        else if (v[mid] < l) {
+        // finding no. of missed ints till mid
+        int missed = v[mid] - (mid + 1);
+        if (missed < k) {
             low = mid + 1;
         } else {
             high = mid - 1;
         }
     }
-    return -1;
-}
-// Brute Force
-int missingK(vector < int > vec, int n, int k) {
-    int cnt = 0;
-    int maxi = vec[vec.size() - 1];
-    for (int i = 1; i < maxi; i++)
-    {
-        if (findNum(vec, i) == -1)
-        {
-            cnt++;
-        }
-        if (cnt == k)
-        {
-            return i;
-        }   
-    }
-    return maxi+(k-cnt);
+    // vec[high] stores the greatest integer lesser
+    // than kth missing num present in vector
+    // thus no. of missed nums till vec[high], missNum = vec[high] - (high + 1)
+    // therefore, kth missed num = vec[high] + k - missNum = k + high + 1;
+    return k + high + 1;
 }
 
 int main()
