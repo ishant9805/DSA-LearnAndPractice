8cc58f0bcbead1794bdca5502eed319b81289ca6
ishant9805 <ishant9805@gmail.com>
2023-12-29 19:05:32 +0530

    Solved with brute force method

---
+++ codingNinja/allocateBooks.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+#include<numeric>
+
+using namespace std;
+
+int bookAlloc(vector<int> &arr, int k) {
+    int stu = 1, pStud = 0;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (pStud + arr[i] <= k)
+        {
+            pStud += arr[i];
+        } else {
+            pStud = arr[i];
+            stu++;
+        }
+    }
+    return stu;
+}
+
+int findPages(vector<int>& arr, int n, int m) {
+    if (m > n) return -1;
+    int maxi = *max_element(arr.begin(), arr.end());
+    int sumo = accumulate(arr.begin(), arr.end(), 0);
+    for (int i = maxi; i < sumo; i++)
+    {
+        if (bookAlloc(arr, i) == m) return i;
+    }
+    return sumo;
+}
+int main()
+{
+    vector<int> v = {12,34,67,90};
+    cout << findPages(v, 4, 2) << endl;
+    vector<int> v2 = {25,46,28,49,24};
+    cout << findPages(v2, 5, 4) << endl;
+    return 0;
+}
