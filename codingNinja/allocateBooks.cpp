f73b4d60948a97da59ef21cf2741aa5c12e3413b
ishant9805 <ishant9805@gmail.com>
2023-12-29 19:55:45 +0530

    Solved

--- codingNinja/allocateBooks.cpp
+++ codingNinja/allocateBooks.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 int bookAlloc(vector<int> &arr, int k) {
-    int stu = 1, pStud = 0;
+    int stu = 1; long long pStud = 0;
     for (int i = 0; i < arr.size(); i++)
     {
         if (pStud + arr[i] <= k)
@@ -18,16 +18,33 @@ int bookAlloc(vector<int> &arr, int k) {
     }
     return stu;
 }
+// Brute Force
+// int findPages(vector<int>& arr, int n, int m) {
+//     if (m > n) return -1;
+//     int maxi = *max_element(arr.begin(), arr.end());
+//     int sumo = accumulate(arr.begin(), arr.end(), 0);
+//     for (int i = maxi; i < sumo; i++)
+//     {
+//         if (bookAlloc(arr, i) == m) return i;
+//     }
+//     return sumo;
+// }
 
-int findPages(vector<int>& arr, int n, int m) {
+// Binary Search 
+int findPages(vector<int> &arr, int n, int m) {
     if (m > n) return -1;
-    int maxi = *max_element(arr.begin(), arr.end());
-    int sumo = accumulate(arr.begin(), arr.end(), 0);
-    for (int i = maxi; i < sumo; i++)
-    {
-        if (bookAlloc(arr, i) == m) return i;
+    int low = *max_element(arr.begin(), arr.end());
+    int high = accumulate(arr.begin(), arr.end(), 0);
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        int st = bookAlloc(arr, mid);
+        if (st > m) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
     }
-    return sumo;
+    return low;
 }
 int main()
 {
