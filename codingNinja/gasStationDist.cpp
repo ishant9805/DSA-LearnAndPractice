a6a2c45d11168501eb3bdc4453568eb3579cf748
ishant9805 <ishant9805@gmail.com>
2024-01-03 19:15:13 +0530

    Solved with help of tutorial

--- codingNinja/gasStationDist.cpp
+++ codingNinja/gasStationDist.cpp
@@ -24,8 +24,44 @@ using namespace std;
 // }
 
 // Binary Search Method
-double minimiseMaxDistance(vector<int> &arr, int k) {
+int maxDiff(vector<int> &arr, int sz) {
+    int ans = INT_MIN;
+    for (int i = 0; i < sz - 1; i++)
+    {
+        int diff = arr[i+1] - arr[i];
+        ans = max(ans, diff);
+    }
+    return ans;
+}
 
+int check(vector<int> &vec, long double dist) {
+    int cnt = 0;
+    for (int i = 1; i < vec.size(); i++) {
+        int numInBet = ((vec[i] - vec[i-1]) / dist);
+        if (vec[i] - vec[i-1] / dist == numInBet * dist) {
+            numInBet--;
+        }
+        cnt += numInBet;
+    }
+    return cnt;
+}
+
+double minimiseMaxDistance(vector<int> &arr, int k) {
+    int n = arr.size();
+    long double low = 0, high = maxDiff(arr, n);
+    long double diff = 1e-6;
+    while (high - low > diff)
+    {
+        long double mid = (high + low) / (2.0);
+        int cnt = check(arr, mid);
+        if (cnt > k)
+        {
+            low = mid;
+        } else {
+            high = mid;
+        }
+    }
+    return high;
 }
 
 int main() {
