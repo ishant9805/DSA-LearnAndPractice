00b50efcfa2ebd0a8789e3b384645030d2599327
ishant9805 <ishant9805@gmail.com>
2024-01-06 20:25:44 +0530

    solved easily

--- codingNinja/kthElement.cpp
+++ codingNinja/kthElement.cpp
@@ -3,40 +3,40 @@
 using namespace std;
 
 // Using merge sort algo
-int kthElement(vector<int> &arr1, vector<int>& arr2, int n, int m, int k){
-    int p1 = 0, p2 = 0;
-    int index = 0;
-    while (p1 < n && p2 < m) {
-        if (arr1[p1] < arr2[p2]) {
-            p1++;
-            index++;
-            if (index == k) {
-                return arr1[p1];
-            }
-        } else {
-            p2++;
-            index++;
-            if (index == k) {
-                return arr2[p2];
-            }
-        }
-    }
-    while (p1 < n) {
-        p1++;
-        index++;
-        if (index == k) {
-            return arr1[p1];
-        }
-    }
-    while (p2 < m) {
-        p2++;
-        index++;
-        if (index == k) {
-            return arr2[p2];
-        }
-    }
-    return -1;
-}
+// int kthElement(vector<int> &arr1, vector<int>& arr2, int n, int m, int k){
+//     int p1 = 0, p2 = 0;
+//     int index = 0;
+//     while (p1 < n && p2 < m) {
+//         if (arr1[p1] < arr2[p2]) {
+//             index++;
+//             if (index == k) {
+//                 return arr1[p1];
+//             }
+//             p1++;
+//         } else {
+//             index++;
+//             if (index == k) {
+//                 return arr2[p2];
+//             }
+//             p2++;
+//         }
+//     }
+//     while (p1 < n) {
+//         index++;
+//         if (index == k) {
+//             return arr1[p1];
+//         }
+//         p1++;
+//     }
+//     while (p2 < m) {
+//         index++;
+//         if (index == k) {
+//             return arr2[p2];
+//         }
+//         p2++;
+//     }
+//     return -1;
+// }
 
 int main() {
     cout << "Starting Program" << endl;
