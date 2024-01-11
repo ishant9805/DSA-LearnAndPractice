6b78f025fb83f7e741fe83fe416d6ea767cbea7a
ishant9805 <ishant9805@gmail.com>
2024-01-11 18:39:14 +0530

    Trying to solve

---
+++ codingNinja/peakGrid.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+vector<int> findPeakGrid(vector<vector<int>> &g){
+
+}
+
+int main() {
+    cout << "Starting Program" << endl;
+    vector<vector<int>> v1 = {{8, 6}, {10, 1}};
+    vector<int> vv1 = findPeakGrid(v1);
+    cout << "------Printing Vector------\nVector: {";
+    for (int i = 0; i < vv1.size() - 1; i++)
+    {
+        cout << vv1[i] << ", ";
+    }
+    cout << vv1[vv1.size() - 1] << "}" << endl;
+    vector<vector<int>> v2 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    vector<int> vv2 = findPeakGrid(v2);
+    cout << "------Printing Vector------\nVector: {";
+    for (int i = 0; i < vv2.size() - 1; i++)
+    {
+        cout << vv2[i] << ", ";
+    }
+    cout << vv2[vv2.size() - 1] << "}" << endl;
+    return 0;
+}
