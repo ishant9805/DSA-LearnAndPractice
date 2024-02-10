507654093f8d54af87a237e7b124319f22a33085
ishant9805 <ishant9805@gmail.com>
2024-02-10 17:57:12 +0530

    SOlved using BS

--- codingNinja/matrixMedian.cpp
+++ codingNinja/matrixMedian.cpp
@@ -2,19 +2,58 @@
 
 using namespace std;
 
-int median(vector<vector<int>> &matrix, int m, int n) {
-    vector<int> flat;
-    for (int i = 0; i < m; i++)
+// Brute force
+// int median(vector<vector<int>> &matrix, int m, int n) {
+//     vector<int> flat;
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             flat.push_back(matrix[i][j]);
+//         }
+        
+//     }
+//     sort(flat.begin(), flat.end());
+//     m = (m*n) / 2;
+//     return flat[m];
+// }
+
+int bbox(vector<vector<int>> &mat, int x) {
+    auto cnt = 0;
+    for (int i = 0; i < mat.size(); i++)
     {
-        for (int j = 0; j < n; j++)
+        cnt += upper_bound(mat[i].begin(), mat[i].end(), x) - mat[i].begin();
+    }
+    return cnt;
+}
+
+int median(vector<vector<int>> &mat, int m, int n) {
+    int low = 1e9;
+    for (int i = 0; i < m; i++) {
+        if (low > mat[i][0]) {
+            low = mat[i][0];
+        }
+    }
+    int high = 0;
+    for (int i = 0; i < m; i++) {
+        if (high < mat[i][n - 1]) {
+            high = mat[i][n - 1];
+        }
+    }
+    int rq = (m * n) / 2;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        int small_eq = bbox(mat, mid);
+        if (small_eq <= rq)
         {
-            flat.push_back(matrix[i][j]);
+            low = mid + 1;
+        } else {
+            high = mid - 1;
         }
         
     }
-    sort(flat.begin(), flat.end());
-    m = (m*n) / 2;
-    return flat[m];
+    return low;
 }
 
 int main() {
