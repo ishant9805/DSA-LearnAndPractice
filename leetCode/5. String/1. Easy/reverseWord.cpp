62e8646cd80ce39b6f60c37d54a5c288571bf9e7
ishant9805 <ishant9805@gmail.com>
2024-02-14 19:42:55 +0530

    Solved

--- leetCode/5. String/1. Easy/reverseWord.cpp
+++ leetCode/5. String/1. Easy/reverseWord.cpp
@@ -1,28 +1,41 @@
 #include<bits/stdc++.h>
 
 using namespace std;
+
+string join(vector<string> s) {
+    string answer;
+    int to = s.size() - 1;
+    for (int i = 0; i < to; i++)
+    {
+        answer.append(s[i]);
+        answer.append(" ");
+    }
+    answer.append(s[to]);
+    return answer;
+}
+
 string reverseWords(string str) {
 	// Write your code here.
-    string sentence = "";
+    vector<string> sentence;
     int length = str.length();
     int count = 0;
     for (int i = length - 1; i >= 0; i--)
     {
         if (str[i] == ' ' && count != 0) {
-            sentence.append(str.substr(i+1, count));
-            sentence.push_back(' ');
+            sentence.push_back(str.substr(i+1, count));
             count = 0;
         } else if (str[i] == ' ' && count == 0) {
             continue;
         } else if (i == 0) {
-            sentence.append(str.substr(0, str.find(' ')));
+            sentence.push_back(str.substr(0, str.find(' ')));
         } else {
             count++;
         }
-        // "I am a star"
+        // "  hello world  "
         //   1  4 5    
     }
-    return sentence;
+    str = join(sentence);
+    return str;
 }
 
 int main() {
