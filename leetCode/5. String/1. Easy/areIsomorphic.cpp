1d71c839949c88341516307beea874e71293bf18
ishant9805 <ishant9805@gmail.com>
2024-02-16 14:37:07 +0530

    Solved Easily

--- leetCode/5. String/1. Easy/areIsomorphic.cpp
+++ leetCode/5. String/1. Easy/areIsomorphic.cpp
@@ -2,45 +2,19 @@
 
 using namespace std;
 
-unordered_map<char, char>::iterator findByValue(unordered_map<char, char> ump, char c) {
-    unordered_map<char, char>::iterator itr;
-    for ( itr = ump.begin(); itr != ump.end(); itr++)
-    {
-        if (itr -> second == c) {
-            return itr;
-        }
-    }
-    return ump.end();
-}
-
 bool areIsomorphic(string &str1, string &str2) {
-    unordered_map<char, char> umpp;
+    map<char, char> mapp;
+    map<char, char> revMapp;
     for (int i = 0; i < str1.length(); i++)
     {
-        // --------------------------------------
-        // unordered_map<char, char>::iterator itr;
-        // cout << "\nAll Elements : \n"; 
-        // for (itr = umpp.begin();  
-        //     itr != umpp.end(); itr++)  
-        // { 
-        //     // itr works as a pointer to  
-        //     // pair<string, double> type  
-        //     // itr->first stores the key part and 
-        //     // itr->second stores the value part 
-        //     cout << itr->first << "  " <<  
-        //             itr->second << endl; 
-        // } 
-        // --------------------------------------
-        if ((umpp.find(str1[i]) == umpp.end()) && (findByValue(umpp, str2[i]) == umpp.end())){
-            umpp.insert(make_pair(str1[i], str2[i]));
-        } else {
-            if ((umpp[str1[i]] == str2[i]) || umpp->findByValue()) {
-                continue;
-            } else {
-                return false;
-            }
+        if (((mapp.find(str1[i]) != mapp.end()) && (mapp[str1[i]] != str2[i])) ||
+            ((revMapp.find(str2[i]) != revMapp.end()) && (revMapp[str2[i]] != str1[i]))){
+            return false;
         }
+        mapp.insert(make_pair(str1[i], str2[i]));
+        revMapp.insert(make_pair(str2[i], str1[i]));
     }
+    
     return true;
 }
 
@@ -48,6 +22,9 @@ int main() {
     cout << "Starting Program" << endl;
     string str1 = "hgvoo";
     string str2 = "bnnff";
+    cout << areIsomorphic(str1, str2) << endl;
+    str1 = "aab";
+    str2 = "xyz";
     cout << areIsomorphic(str1, str2) << endl;
     return 0;
 }
