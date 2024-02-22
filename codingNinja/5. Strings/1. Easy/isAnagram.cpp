a87c090330e6c6fde8801adef965dcecc39feae0
ishant9805 <ishant9805@gmail.com>
2024-02-22 13:28:55 +0530

    Solved

---
+++ codingNinja/5. Strings/1. Easy/isAnagram.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+bool isAnagram(string str1, string str2)
+{
+    map<char, int> s1, s2;
+    for (int i = 0; i < str1.length(); i++)
+    {
+        if (s1.find(str1[i]) != s1.end())
+        {
+            s1[str1[i]]++;
+        } else {
+            s1.insert(make_pair(str1[i], 1));
+        }
+    }
+    for (int i = 0; i < str2.length(); i++)
+    {
+        if (s2.find(str2[i]) != s2.end())
+        {
+            s2[str2[i]]++;
+        } else {
+            s2.insert(make_pair(str2[i], 1));
+        }
+    }
+    for (auto itr = s1.begin(); itr != s1.end(); ++itr) {
+      auto ss = s2.find(itr->first);
+      if (ss == s2.end()) return false;
+      if (ss->second != itr->second) return false;
+    }
+    
+    for (auto itr = s2.begin(); itr != s2.end(); ++itr) {
+      auto ss = s1.find(itr->first);
+      if (ss == s1.end()) return false;
+      if (ss->second != itr->second) return false;
+    }
+    return true;
+}
+
+int main() {
+    cout << "Starting Program" << endl;
+    string a = "listen";
+    string b = "silent";
+    cout << isAnagram(a, b) << endl;
+    a = "rasp", b = "spaz";
+    cout << isAnagram(a, b) << endl;
+    return 0;
+}
