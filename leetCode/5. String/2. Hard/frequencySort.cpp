b289f78cdc34faeeda73930c50ef8040010a0872
ishant9805 <ishant9805@gmail.com>
2024-02-25 20:04:53 +0530

    Solved after watching video, check the code for video link

---
+++ leetCode/5. String/2. Hard/frequencySort.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+string frequencySort(string& s)
+{
+    unordered_map<char, int> mpp;
+    priority_queue<pair<int, char>> pq;
+    string res = "";
+    for (char ch:s)
+    {
+        mpp[ch]++;
+    }
+    for (auto it:mpp) {
+        pq.push({it.second, it.first});
+    }
+    while (!pq.empty())
+    {
+        auto tmp = pq.top();
+        int fq = tmp.first;
+        char ch = tmp.second;
+        res += string(fq, ch);
+        pq.pop();
+    }
+    return res;
+}
+
+
+int main() {
+    cout << "Starting Program" << endl;
+    string s = "tree";
+    s = frequencySort(s);
+    cout << s << endl;
+    
+    return 0;
+}
