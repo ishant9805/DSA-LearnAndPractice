7eedcf02d25355ebec37a9bc8fca1ba98ab4ad48
ishant9805 <ishant9805@gmail.com>
2024-03-10 13:02:09 +0530

    Solved after tutorial

---
+++ leetCode/6. Linked List/1. Easy/deleteNode.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+void deleteNode(ListNode* node) {
+    node->val = node->next->val;
+    node->next = node->next->next;
+}
+
+int main() {
+    cout << "Starting Program" << endl;
+    
+    return 0;
+}
