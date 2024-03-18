66a9259da684bb0b9991faec6e32c4cd0ff9935f
ishant9805 <ishant9805@gmail.com>
2024-03-18 23:37:38 +0530

    Learnt to find starting point of a cycle in LL

---
+++ codingNinja/6. Linked List/3. Medium Problems(LL)/firstNode.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node()
+    {
+        this->data = 0;
+        next = NULL;
+    }
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+    Node(int data, Node* next)
+    {
+        this->data = data;
+        this->next = next;
+    }
+};
+
+Node *firstNode(Node *head){
+    Node* slow = head;
+    Node* fast = head;
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast) {
+            slow = head;
+            while (slow != fast)
+            {
+                slow = slow->next;
+                fast = fast->next;
+            }
+            return slow;
+        }
+    }
+    return NULL;
+}
+
+int main() {
+    cout << "Starting Program" << endl;
+    vector<int> v1 = {};
+    
+    return 0;
+}
