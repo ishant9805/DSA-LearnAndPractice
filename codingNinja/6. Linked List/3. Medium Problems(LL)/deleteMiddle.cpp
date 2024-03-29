294e164938d7b0ba829d4a182c9c4a07c90fabe8
ishant9805 <ishant9805@gmail.com>
2024-03-29 11:41:36 +0530

    Solved

---
+++ codingNinja/6. Linked List/3. Medium Problems(LL)/deleteMiddle.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node()
+    {
+        this->data = 0;
+        next = NULL;
+    }
+    Node(int data)
+    {
+        this->data = data; 
+        this->next = NULL;
+    }
+    Node(int data, Node* next)
+    {
+        this->data = data;
+        this->next = next;
+    }
+};
+
+Node* constructLL(vector<int>& arr);
+void PrintLL(Node* head);
+
+Node* deleteMiddle(Node* head) {
+    if (head == NULL || head->next == NULL) {
+        return NULL;
+    }
+    Node* fast = head;
+    Node* slow = head;
+    Node* prev;
+    while (fast && fast->next) {
+        prev = slow;
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    prev->next = slow->next;
+    delete slow;
+    return head;
+}
+
+int main() {
+    cout << "Starting Program" << endl;
+    vector<int> v1 = {1,2,3,4,5};
+    Node* head = constructLL(v1);
+    PrintLL(head);
+    head = deleteMiddle(head);
+    PrintLL(head);
+    return 0;
+}
+
+Node* constructLL(vector<int>& arr) {
+    int len = arr.size();
+    Node* prev = new Node(arr[0], nullptr);
+    Node* head = prev;
+    for (int i = 1; i < len; i++) {
+        Node *temp = new Node(arr[i]);
+        prev -> next = temp;
+        prev = temp;
+    }
+    return head;
+}
+
+void PrintLL(Node* head) {
+    if (head == nullptr) {
+        cout << "There is nothing in LL to print🙂" << endl;
+    } else {
+        cout << "[";
+        while(head->next != nullptr) {
+            cout << head->data << ", ";
+            head = head->next;
+        }
+        cout << head->data << "]" << endl;
+    }
+}
---
+++ leetCode/6. Linked List/1. Easy/deleteMiddle.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+ListNode* constructLL(vector<int>& arr);
+void PrintLL(ListNode* head);
+
+ListNode* deleteMiddle(ListNode* head) {
+    if (head == NULL || head->next == NULL) {
+        return NULL;
+    }
+    ListNode* fast = head;
+    ListNode* slow = head;
+    ListNode* prev;
+    while (fast && fast->next) {
+        prev = slow;
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    prev->next = slow->next;
+    delete slow;
+    return head;
+}
+
+int main() {
+    cout << "Starting Program" << endl;
+    vector<int> v1 = {1,2,3,4,5};
+    ListNode* head = constructLL(v1);
+    PrintLL(head);
+    head = deleteMiddle(head);
+    PrintLL(head);
+    return 0;
+}
+
+ListNode* constructLL(vector<int>& arr) {
+    int len = arr.size();
+    ListNode* prev = new ListNode(arr[0], nullptr);
+    ListNode* head = prev;
+    for (int i = 1; i < len; i++) {
+        ListNode *temp = new ListNode(arr[i]);
+        prev -> next = temp;
+        prev = temp;
+    }
+    return head;
+}
+
+void PrintLL(ListNode* head) {
+    if (head == nullptr) {
+        cout << "There is nothing in LL to print🙂" << endl;
+    } else {
+        cout << "[";
+        while(head->next != nullptr) {
+            cout << head->val << ", ";
+            head = head->next;
+        }
+        cout << head->val << "]" << endl;
+    }
+}
