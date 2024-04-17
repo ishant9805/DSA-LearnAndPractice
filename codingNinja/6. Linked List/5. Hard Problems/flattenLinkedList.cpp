f477a527eda1f5f83fb49211b40bcff3e0f944a1
ishant9805 <ishant9805@gmail.com>
2024-04-17 22:38:57 +0530

    Learnt flattening

--- codingNinja/6. Linked List/5. Hard Problems/flattenLinkedList.cpp
+++ codingNinja/6. Linked List/5. Hard Problems/flattenLinkedList.cpp
@@ -10,56 +10,33 @@ class Node {
 	Node(int x) : data(x), next(nullptr), child(nullptr) {}
 	Node(int x, Node *next, Node *child) : data(x), next(next), child(child) {}
 };
-Node* constructLL(vector<int>& arr);
-void PrintLL(Node* head);
 
 Node* flattenLinkedList(Node* head) 
 {
 	Node* tmp = head;
-    Node* nH = tmp;
-    Node* it = nH;
+    vector<int> arr;
     while(tmp) {
-        Node *child = tmp->child;
-        while(child->next){
-            child = child->next;
+        Node *ch = tmp;
+        while(ch) {
+            arr.push_back(ch->data);
+            ch = ch->child;
         }
-        it->next = tmp->child;
-        child->next = tmp->next;
         tmp = tmp->next;
-        it = tmp;
     }
-    return nH;
+    sort(arr.begin(), arr.end());
+    if (arr.size() == 0) return NULL;
+    Node* newH = new Node(arr[0]);
+    tmp = newH;
+    for(int i = 1; i < arr.size(); i++) {
+        Node *hh = new Node(arr[i]);
+        tmp->child = hh;
+        tmp = tmp->child;
+    }
+    return newH;
 }
 
 int main() {
     cout << "Starting Program" << endl;
-    vector<vector<int>> v1 = {{1, 2, 3}, {4, 5, 6}, {7,8}, {9,12}, {20}};
-    Node* num1 = constructLL(v1);
-    PrintLL(num1);
+    cout << "NO DRIVER CODE PRESENT" << endl;
     return 0;
-}
-
-Node* constructLL(vector<vector<int>>& arr) {
-    int len = arr.size();
-    Node* prev = new Node(arr[0], nullptr, nullptr);
-    Node* head = prev;
-    for (int i = 1; i < len; i++) {
-        Node *temp = new Node(arr[i], nullptr, nullptr);
-        prev -> next = temp;
-        prev = temp;
-    }
-    return head;
-}
-
-void PrintLL(Node* head) {
-    if (head == nullptr) {
-        cout << "There is nothing in LL to print🙂" << endl;
-    } else {
-        cout << "[";
-        while(head->next != nullptr) {
-            cout << head->data << ", ";
-            head = head->next;
-        }
-        cout << head->data << "]" << endl;
-    }
 }
