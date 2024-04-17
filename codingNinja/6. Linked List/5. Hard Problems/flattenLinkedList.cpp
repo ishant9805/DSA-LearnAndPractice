93d0cfead61176d0fef19fa1cff3cc16ce12f83a
ishant9805 <ishant9805@gmail.com>
2024-04-17 23:58:19 +0530

    Learnt to use recursion in LL

--- codingNinja/6. Linked List/5. Hard Problems/flattenLinkedList.cpp
+++ codingNinja/6. Linked List/5. Hard Problems/flattenLinkedList.cpp
@@ -11,28 +11,61 @@ class Node {
 	Node(int x, Node *next, Node *child) : data(x), next(next), child(child) {}
 };
 
-Node* flattenLinkedList(Node* head) 
-{
-	Node* tmp = head;
-    vector<int> arr;
-    while(tmp) {
-        Node *ch = tmp;
-        while(ch) {
-            arr.push_back(ch->data);
-            ch = ch->child;
-        }
-        tmp = tmp->next;
+// Naive Solution
+// Node* flattenLinkedList(Node* head) 
+// {
+// 	Node* tmp = head;
+//     vector<int> arr;
+//     while(tmp) {
+//         Node *ch = tmp;
+//         while(ch) {
+//             arr.push_back(ch->data);
+//             ch = ch->child;
+//         }
+//         tmp = tmp->next;
+//     }
+//     sort(arr.begin(), arr.end());
+//     if (arr.size() == 0) return NULL;
+//     Node* newH = new Node(arr[0]);
+//     tmp = newH;
+//     for(int i = 1; i < arr.size(); i++) {
+//         Node *hh = new Node(arr[i]);
+//         tmp->child = hh;
+//         tmp = tmp->child;
+//     }
+//     return newH;
+// }
+
+// Merge method
+// Merge method
+Node* merge(Node* list1, Node* list2) {
+    if (list1 == NULL) {
+        list2->next = nullptr;
+        return list2;
     }
-    sort(arr.begin(), arr.end());
-    if (arr.size() == 0) return NULL;
-    Node* newH = new Node(arr[0]);
-    tmp = newH;
-    for(int i = 1; i < arr.size(); i++) {
-        Node *hh = new Node(arr[i]);
-        tmp->child = hh;
-        tmp = tmp->child;
+    if (list2 == NULL) {
+        list1->next = nullptr;
+        return list1;
     }
-    return newH;
+    Node* merg = NULL;
+    if(list2->data < list1->data) {
+        merg = list2;
+        merg->child = merge(list2->child, list1);
+    } else {
+        merg = list1;
+        merg->child = merge(list1->child, list2);
+    }
+    merg->next = nullptr;
+    return merg;
+}
+
+Node* flattenLinkedList(Node* head) {
+    if (head == NULL || head->next == NULL) {
+        return head;
+    }
+    head->next = flattenLinkedList(head->next);
+    head = merge(head, head->next);
+    return head;
 }
 
 int main() {
