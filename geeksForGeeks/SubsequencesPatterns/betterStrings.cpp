fefd3a2ce58c5d3f6210f0e72adbaebb795842ae
ishant9805 <ishant9805@gmail.com>
2024-05-09 23:51:42 +0530

    Trying to solve

---
+++ geeksForGeeks/SubsequencesPatterns/betterStrings.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+string betterString(string str1, string str2) {
+    // code here
+}
+
+int main() {
+    cout << "Starting Program" << endl;
+
+    return 0;
+}
