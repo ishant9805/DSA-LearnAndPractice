93a2ff8d832c7a6c90aa2c65a2cb9878522d50a4
ishant9805 <ishant9805@gmail.com>
2025-07-22 23:56:08 +0530

    Solved (with another approach)

--- revision/findSimilarity.cpp
+++ revision/findSimilarity.cpp
@@ -1,26 +1,43 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include<unordered_set>
 
 using namespace std;
 
 
+// pair < int , int > findSimilarity(vector < int > arr1, vector < int > arr2, int n, int m) 
+// {
+//     int same = 0;
+// 	for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j < m; j++)
+//         {
+//             if (arr1[i] == arr2[j]) {
+//                 same++;
+//             }
+//         }
+        
+//     }
+//     return {same, m + n - same};  
+// }
+
+
 pair < int , int > findSimilarity(vector < int > arr1, vector < int > arr2, int n, int m) 
 {
-    int same = 0;
-	for (int i = 0; i < n; i++)
+    unordered_set<int> uset;
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < m; j++)
-        {
-            if (arr1[i] == arr2[j]) {
-                same++;
-            }
-        }
-        
+        uset.insert(arr1[i]);
     }
-    return {same, m + n - same};  
-}
+    for (int i = 0; i < m; i++)
+    {
+        uset.insert(arr2[i]);
+    }
 
+    return {m + n -uset.size() , uset.size()};
+    
+}
 
 int main()
 {
